fed323244858c34659358dc2bea8fab1218bbc03
firman742 <hafidzfirman742@gmail.com>
2024-10-27 18:39:00 +0700

    Update Index.html

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Zidnan Bimbel</title>
+    <title>Zidnan Bimbel - Home</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <style>
         /* Gaya Umum */
